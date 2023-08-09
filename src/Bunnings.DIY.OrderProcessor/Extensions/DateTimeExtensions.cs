bd0c82b4c3c595fd5d56877b92e541ae569694fa
Cheranga <cheranga@gmail.com>
2023-08-09 19:57:45 +1000

    chore: reading and publishing messages to queue [ci]

---
+++ src/Bunnings.DIY.OrderProcessor/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Bunnings.DIY.OrderProcessor.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static TimeSpan ToTimeSpan(this int seconds) => TimeSpan.FromSeconds(seconds);
+}
---
+++ src/Bunnings.DIY.OrderProcessor/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Bunnings.DIY.OrderProcessor;
+using Bunnings.DIY.OrderProcessor.Features.ReadFile;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Bunnings.DIY.OrderProcessor;
+
+public class Startup : FunctionsStartup
+{
+    protected virtual IConfiguration GetConfiguration(IFunctionsHostBuilder builder)
+    {
+        var executionContextOptions = builder.Services
+            .BuildServiceProvider()
+            .GetService<IOptions<ExecutionContextOptions>>()
+            .Value;
+
+        return new ConfigurationBuilder()
+            .SetBasePath(executionContextOptions.AppDirectory)
+            .AddJsonFile("local.settings.json", true)
+            .AddEnvironmentVariables()
+            .Build();
+    }
+    
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        var services = builder.Services;
+
+        services.AddSingleton(_ =>
+        {
+            var config = GetConfiguration(builder).GetSection(nameof(ReadFileConfig)).Get<ReadFileConfig>();
+            return config;
+        });
+    }
+}
