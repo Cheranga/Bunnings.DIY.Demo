ee6c48694b681fbeab3408ae0db48f295f6ce8f8
Cheranga <cheranga@gmail.com>
2023-08-10 14:17:49 +1000

    chore: adding nuke generated ci pipeline [ci]

--- build/Build.cs
+++ build/Build.cs
@@ -1,7 +1,14 @@
 using _build;
 using Nuke.Common;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.ProjectModel;
 
+[GitHubActions(
+    "bunnings-diy",
+    GitHubActionsImage.UbuntuLatest,
+    On = new[] { GitHubActionsTrigger.PullRequest },
+    InvokedTargets = new[] { nameof(IDotnetBuildTasks.Compile) }
+)]
 sealed class Build : NukeBuild, IDotnetBuildTasks, IFormatCodeTasks, INpmBuildTasks, IOasLintTask
 {
     public static int Main() => Execute<Build>(x => (x as IDotnetBuildTasks).Compile);
